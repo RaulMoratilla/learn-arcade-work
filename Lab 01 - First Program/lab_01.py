9c4bee57d43099eb520feb62d3df390927a8f305
RaulMoratilla <raul.moratilla@edu.uah.es>
2021-02-18 20:09:20 +0100

    lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-print("Hola que tal todo bien todo correcto.\nY yo que me alegro")
+print('I hope my day finishes soon.')
+print('I\'ve been the hole day studing.')
+print('Just now I just want to sleep.')
+print('Anyway, it worth it to be a prepared man.\nGreatful to like my degree.')
